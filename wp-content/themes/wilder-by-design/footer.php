5b06088529155a367c53f6749f4472bdeef92f21
jaceuk <jnewington@gmail.com>
2024-04-28 13:04:59 +0100

    v2

--- wp-content/themes/wilder-by-design/footer.php
+++ wp-content/themes/wilder-by-design/footer.php
@@ -22,20 +22,14 @@
 
 
 
-		<!-- <div class="newsletter">
+		<div class="newsletter">
 			<h2>Signup for exclusive deals</h2>
-			<p>Sign up to our newsletter to stay up to date on new arrivals and get exclusive offers. You can cancel at any time.</p> -->
+			<p>Sign up to our newsletter to stay up to date on new arrivals and get exclusive offers. You can cancel at any time.</p>
 
-		<?php
-		// if (file_exists('c:\windows')) {
-		// 	$form_id = 993;
-		// } else {
-		// 	$form_id = 1007;
-		// }
-
-		// echo do_shortcode('[wpforms id=' . $form_id . ']');
-		?>
-		<!-- </div> -->
+			<?php
+			echo do_shortcode('[wpforms id=1007]');
+			?>
+		</div>
 
 		<div class="legals">
 			<ul>
