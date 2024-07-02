6612e6545716cc6084367179031e4d5ecfcf6fe8
jaceuk <jnewington@gmail.com>
2024-07-02 22:49:59 +0100

    Collection changes

--- wp-content/themes/wilder-by-design/components/collections.php
+++ wp-content/themes/wilder-by-design/components/collections.php
@@ -4,52 +4,36 @@ $collections_title = get_field('collections_title');
 ?>
 
 <header>
-  <?php the_title('<h1>', '</h1>'); ?>
+  <?php the_title('<h1 class="collections-header">', '</h1>'); ?>
 </header>
 
 <?php
-$terms = get_terms(array(
-  'taxonomy' => 'product_tag',
+$category = get_term_by('slug', 'collections', 'product_cat');
+$cat_id = $category->term_id;
+$cats = get_categories(array(
+  'taxonomy' => 'product_cat',
   'orderby' => 'date',
-  'order'   => 'ASC'
+  'order'   => 'ASC',
+  'parent' => $cat_id,
 ));
 
 echo '<div class="collections">';
 
-$collection_ids = [];
-if (!empty($terms) && !is_wp_error($terms)) {
-  foreach ($terms as $term) {
-    $term_type = get_field('tag_type',  $term->taxonomy . '_' . $term->term_id);
-
-    if (strtolower($term_type) === 'collection') {
-      $term_meta = get_term_meta($term->term_id);
-      $url = get_tag_link($id);
-
-      $args = array(
-        'posts_per_page' => -1,
-        'post_type' => 'product',
-        'product_tag' => $term->slug
-      );
-      $query = new WP_Query($args);
-
-      $count = $query->post_count;
+foreach ($cats as $cat) {
 ?>
-      <section class="products-section archive">
-        <div>
-          <div class="title">
-            <h2><?php echo $term->name; ?></h2>
-            <div class="divider"></div>
-            <a href="/product-tag/<?php echo $term->slug; ?>" class="more">View all (<?php echo $count; ?> results)</a>
-          </div>
-          <?php
-          echo do_shortcode('[products limit="4" columns="4" orderby="popularity" tag="' . $term->slug . '"]');
-          ?>
-        </div>
-      </section>
+  <section class="products-section archive">
+    <div>
+      <div class="title">
+        <h2><?php echo $cat->name; ?></h2>
+        <div class="divider"></div>
+        <a href="/product-tag/<?php echo $cat->slug; ?>" class="more">View all (<?php echo $cat->count; ?> results)</a>
+      </div>
+      <?php
+      echo do_shortcode('[products limit="4" columns="4" orderby="popularity" category="' . $cat->slug . '"]');
+      ?>
+    </div>
+  </section>
 <?php
-    }
-  }
 }
-
 echo '</div>';
 ?>
--- wp-content/themes/wilder-by-design/components/featured-collections.php
+++ wp-content/themes/wilder-by-design/components/featured-collections.php
@@ -1,41 +1,43 @@
-<?php
-$collections = get_field('collections');
-$collections_title = get_field('collections_title');
-?>
-
 <section class="home-section products-section">
   <div class="inner-wrapper">
     <div class="title">
-      <h2><?php echo $collections_title; ?></h2>
+      <h2>Latest Collections</h2>
       <div class="divider"></div>
       <a href="/collections" class="more">View all</a>
     </div>
 
-    <div class="col-2">
-      <?php
-      $size = 'large';
+    <?php
+    $category = get_term_by('slug', 'collections', 'product_cat');
+    $cat_id = $category->term_id;
+    $cats = get_categories(array(
+      'taxonomy' => 'product_cat',
+      'orderby' => 'date',
+      'order'   => 'ASC',
+      'parent' => $cat_id,
+      'number' => 4,
+    ));
 
-      foreach ($collections as $collection) {
-        $id = $collection['collection'];
-        $term_name = get_term($id)->name;
-        $term_meta = get_term_meta($id);
-        $url = get_tag_link($id);
-        $image_id = $term_meta['image'][0];
-      ?>
-        <a class="featured-collection-card" href="<?php echo $url; ?>">
-          <div class="image">
-            <?php
-            if (!empty($image_id)) {
-              echo wp_get_attachment_image($image_id, $size);
-            }
-            ?>
-          </div>
-          <h3 class="body"><?php echo $term_name; ?></h3>
-        </a>
-      <?php
-      }
-      ?>
-    </div>
+    echo '<div class="col-4">';
+
+    $size = 'large';
+
+    foreach ($cats as $cat) {
+      $thumbnail_id = get_term_meta($cat->term_id, 'thumbnail_id', true);
+    ?>
+      <a class="personalised-card" href="/product-category/collections/<?php echo $cat->slug; ?>">
+        <div class="image">
+          <?php
+          if (!empty($thumbnail_id)) {
+            echo wp_get_attachment_image($thumbnail_id, $size);
+          }
+          ?>
+        </div>
+        <h3 class="body"><?php echo $cat->name; ?></h3>
+      </a>
+    <?php
+    }
+    echo '</div>';
+    ?>
   </div>
   </div>
 </section>
