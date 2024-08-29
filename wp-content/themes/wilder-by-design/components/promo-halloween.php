06452445493c18c9afc9f836f5b18e3741aac671
jaceuk <jnewington@gmail.com>
2024-08-29 22:25:24 +0100

    Halloween tweaks

--- wp-content/themes/wilder-by-design/components/promo-halloween.php
+++ wp-content/themes/wilder-by-design/components/promo-halloween.php
@@ -1,17 +1,11 @@
 <div class="promo-row inner-wrapper">
-  <section class="promo halloween-promo">
+  <a class="promo halloween-promo" href="/product-tag/funny-halloween-t-shirts/">
     <img src="<?php echo get_stylesheet_directory_uri() ?>/images/promo-halloween-humour.jpg" alt="" />
-    <div class="title">
-      <h2>Halloween Humour</h2>
-      <a class="button" href="/product-tag/funny-halloween-t-shirts/">Shop now</a>
-    </div>
-  </section>
+    <h2>Halloween Humour</h2>
+  </a>
 
-  <section class="promo halloween-promo">
+  <a class="promo halloween-promo" href="/product-tag/wolf-cursed-howl-of-the-night/">
     <img src="<?php echo get_stylesheet_directory_uri() ?>/images/promo-cursed-howl.jpg" alt="" />
-    <div class="title">
-      <h2>Cursed Howl of the Night</h2>
-      <a class="button" href="/product-tag/wolf-cursed-howl-of-the-night/">Shop now</a>
-    </div>
-  </section>
+    <h2>Cursed Howl of the Night</h2>
+  </a>
 </div>
